58e2c28632ffe0c7f476921842de6116d4082e72
Grant Forrest <gforrest@bandwidth.com>
2017-03-14 09:30:51 -0400

    fix bad path

--- components/MultiSelectGrid/MultiSelectGridItem.js
+++ components/MultiSelectGrid/MultiSelectGridItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import childrenWithProps from '../extensions/childrenWithProps';
+import childrenWithProps from '../../extensions/childrenWithProps';
 
 export default class MultiSelectGridItem extends React.Component {
   static propTypes = {
