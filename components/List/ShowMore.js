70052578b002861268005b969ef922aec4538093
Grant Forrest <gforrest@bandwidth.com>
2017-05-11 14:06:57 -0400

    List pagination styling tweak

--- components/List/ShowMore.js
+++ components/List/ShowMore.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ShowMore = styled.div`
   background: ${({ theme }) => theme.colors.primaryLight};
   color: ${({ theme }) => theme.colors.primaryText};
-  padding: ${({ theme }) => theme.padding.large};
+  padding: ${({ theme }) => `${theme.padding.medium} ${theme.padding.large}`};
   cursor: pointer;
 `;
 
