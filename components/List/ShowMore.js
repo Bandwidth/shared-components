1854bdd78e934f5be34b0b0ad67edf9b4c6179ae
Grant Forrest <gforrest@bandwidth.com>
2017-05-15 14:31:30 -0400

    Fix list item border

--- components/List/ShowMore.js
+++ components/List/ShowMore.js
@@ -4,6 +4,8 @@ const ShowMore = styled.div`
   background: ${({ theme }) => theme.colors.primaryLight};
   color: ${({ theme }) => theme.colors.primaryText};
   padding: ${({ theme }) => `${theme.padding.medium} ${theme.padding.large}`};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderLight};
+  border-right: 1px solid ${({ theme }) => theme.colors.borderLight};
   cursor: pointer;
 `;
 
