38a52829369995a13f63dfc34b19773a2be6f007
Grant Forrest <gforrest@bandwidth.com>
2017-05-18 14:25:10 -0400

    Provinces helper funciton

---
+++ extensions/provinces.js
@@ -0,0 +1,14 @@
+import provinceData from 'provinces';
+
+/**
+ * Provides a list of short-code provinces (states, etc) for the country codes provided.
+ */
+export default (countries) => countries.reduce((regions, countryCode) =>
+  regions.concat(
+    regions,
+    provinceData
+      .filter((province) => province.country === countryCode.toUpperCase())
+      .map((province) => province.short),
+  ),
+  [],
+);
