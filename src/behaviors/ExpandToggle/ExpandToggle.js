0de39d197375d866063c673e731801735d4b9e11
Grant Forrest <gforrest@bandwidth.com>
2017-08-31 11:30:04 -0400

    Add more usage docs

--- src/behaviors/ExpandToggle/ExpandToggle.js
+++ src/behaviors/ExpandToggle/ExpandToggle.js
@@ -106,7 +106,14 @@ ExpandToggle.usage = `
   toggleContent={<Label>Click me</Label>}
   startExpanded={false}
 >
+  Stuff!
+</ExpandToggle>
 
+<ExpandToggle
+  isExpanded={true}
+  toggleContent={<Label>Won't do anything</Label>}
+>
+  This one won't toggle, expand state is overridden to true!
 </ExpandToggle>
 \`\`\`
 `;
