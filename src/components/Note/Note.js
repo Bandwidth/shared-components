16604e507d334392bafc12c1b188ecd2519ad1bd
a-type <gforrest@bandwidth.com>
2017-11-30 15:37:04 -0500

    Make visual components replaceable

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NoteBody from './styles/NoteBody';
 import NoteCorner from './styles/NoteCorner';
 
-export default props => (
-  <NoteBody>
-    {props.children}
-    <NoteCorner />
-  </NoteBody>
+const Note = ({ children, Body, Corner }) => (
+  <Body>
+    {children}
+    <Corner />
+  </Body>
 );
+
+Note.propTypes = {
+  /**
+   * Contents of the note
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   * Override the component that renders the body container
+   */
+  Body: PropTypes.func,
+  /**
+   * Override the component that renders the corner
+   */
+  Corner: PropTypes.func,
+};
+
+Note.defaultProps = {
+  Body: NoteBody,
+  Corner: NoteCorner,
+};
+
+export default Note;
