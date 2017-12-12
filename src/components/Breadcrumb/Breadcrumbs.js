1387640fadca186cf23dffb8d0a432e1bc9e7fab
a-type <gforrest@bandwidth.com>
2017-12-12 16:36:12 -0500

    prettier updates on unrelated files

--- src/components/Breadcrumb/Breadcrumbs.js
+++ src/components/Breadcrumb/Breadcrumbs.js
@@ -3,23 +3,27 @@ import Breadcrumb from './Breadcrumb';
 import dedent from 'dedent';
 
 const Breadcrumbs = styled.div`
-  &>${Breadcrumb}:not(:last-child)::after {
-    content: "\00a0 \00a0 ›\00a0 \00a0"
+  & > ${Breadcrumb}:not(:last-child)::after {
+    content: '\00a0 \00a0 ›\00a0 \00a0';
   }
 `;
 
 Breadcrumbs.propTypes = {
-  children: (props) => {
+  children: props => {
     if (!props.children) return;
-    const invalidChildren = props.children.filter(child => child.type.displayName !== Breadcrumb.displayName);
+    const invalidChildren = props.children.filter(
+      child => child.type.displayName !== Breadcrumb.displayName,
+    );
     if (invalidChildren.length > 0) {
       const child = invalidChildren[0];
       const childName = child.type.displayName || child.type.name || child.type;
-      return new Error(dedent`Invalid child component for Breadcrumbs: ${childName}.  Only ${Breadcrumb.displayName} \
+      return new Error(dedent`Invalid child component for Breadcrumbs: ${
+        childName
+      }.  Only ${Breadcrumb.displayName} \
         components may be nested within Breadcrumbs.`);
     }
-  }
-}
+  },
+};
 
 Breadcrumbs.Breadcrumb = Breadcrumb;
 
