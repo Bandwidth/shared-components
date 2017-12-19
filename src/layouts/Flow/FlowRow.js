01477339c31cc714b807687d548232d45dc0ba6c
a-type <gforrest@bandwidth.com>
2017-12-19 14:42:13 -0500

    prettier updates

--- src/layouts/Flow/FlowRow.js
+++ src/layouts/Flow/FlowRow.js
@@ -35,9 +35,7 @@ class FlowRow extends React.Component {
      */
     alignment: (props, propName, componentName) => {
       if (props.sizes.length > 0 && props[propName] !== 'stretch') {
-        return new Error(`Using ${propName} with sizes is invalid in ${
-          componentName
-        }.
+        return new Error(`Using ${propName} with sizes is invalid in ${componentName}.
         ${propName} is ${JSON.stringify(
           props[propName],
         )}, sizes is ${JSON.stringify(props.sizes)}`);
@@ -45,9 +43,7 @@ class FlowRow extends React.Component {
 
       if (!['left', 'right', 'stretch', 'center'].includes(props[propName])) {
         return new Error(
-          `Invalid prop ${propName} supplied to ${
-            componentName
-          }: must be one of [left, right, center, stretch].`,
+          `Invalid prop ${propName} supplied to ${componentName}: must be one of [left, right, center, stretch].`,
         );
       }
     },
