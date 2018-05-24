4c2f940ff11ec1087b076f212b7ea4207bbc53de
a-type <gforrest@bandwidth.com>
2018-05-24 16:06:52 -0400

    Add alternate Note color

--- src/components/Note/styles/NoteContainer.js
+++ src/components/Note/styles/NoteContainer.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import get from 'extensions/themeGet';
+import Corner from './NoteCorner';
 
 const NoteContainer = styled.div`
   background: ${get('colors.background.default')};
@@ -10,6 +11,26 @@ const NoteContainer = styled.div`
   padding-left: 1em;
   min-width: 10em;
   position: relative;
+`;
+
+NoteContainer.Alternate = styled(NoteContainer)`
+  background: ${get('colors.primary.light')};
+  color: ${get('colors.primary.dark')};
+  border-color: ${get('colors.primary.default')};
+
+  & > ${Corner} {
+    border-color: ${get('colors.primary.default')};
+
+    &::before {
+      border-color: ${get('colors.primary.default')} transparent transparent
+        ${get('colors.primary.default')};
+      box-shadow: 0 -1px 0 ${get('colors.primary.default')};
+    }
+    &::after {
+      border-color: ${get('colors.primary.light')} transparent transparent
+        ${get('colors.primary.light')};
+    }
+  }
 `;
 
 export default NoteContainer;
