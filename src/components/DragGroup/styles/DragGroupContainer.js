f526ac4266aff10a5ca56c117a971483b56bcf36
Zack Litzsinger <zlitzsinger@bandwidth.com>
2018-06-05 10:31:39 -0400

    spacing alteration to drag group list

--- src/components/DragGroup/styles/DragGroupContainer.js
+++ src/components/DragGroup/styles/DragGroupContainer.js
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 import get from 'extensions/themeGet';
 
 export default styled.div`
-  padding-bottom: ${get('spacing.small')};
+  & + & {
+    padding-top: ${get('spacing.small')};
+  }
 `;
