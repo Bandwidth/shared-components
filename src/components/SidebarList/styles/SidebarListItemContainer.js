57b83fbb469d81d464e435841a3e8199478620e5
Zack Litzsinger <zlitzsinger@bandwidth.com>
2018-06-15 15:38:55 -0400

    removed blue color from active sidebar list item

--- src/components/SidebarList/styles/SidebarListItemContainer.js
+++ src/components/SidebarList/styles/SidebarListItemContainer.js
@@ -6,10 +6,7 @@ export default styled.li`
     props.active
       ? get('colors.gray.light')(props)
       : get('colors.background.default')(props)};
-  color: ${props =>
-    props.active
-      ? get('colors.primary.default')(props)
-      : get('colors.text.default')(props)};
+  color: ${get('colors.text.default')};
   padding: ${get('spacing.medium')} ${get('spacing.large')};
   border-bottom: ${get('thicknesses.normal')} solid
     ${get('colors.border.light')};
