5ada4418af87a24f080fc4f1d4a5b06bd127c295
Denys Yefremov <denys.yefremov@gmail.com>
2018-08-30 11:00:39 +0300

    Color for required mark now is coming from default colors

--- packages/shared-components/src/components/Label/Label.js
+++ packages/shared-components/src/components/Label/Label.js
@@ -27,7 +27,7 @@ const Label = styled.label.withConfig({ displayName: 'Label' }).attrs({
       ? css`
           &::after {
             content: '*';
-            color: #e8562e;
+            color: ${get('colors.negative.default')}
             padding-left: 0.3em;
           }
         `
