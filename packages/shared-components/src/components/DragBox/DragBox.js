4f64f1868c8bfabdbd42c8ff73824b9561e5f560
Grant Forrest <gforrest@bandwidth.com>
2018-09-06 09:05:29 -0400

    Fix field name

--- packages/shared-components/src/components/DragBox/DragBox.js
+++ packages/shared-components/src/components/DragBox/DragBox.js
@@ -319,7 +319,7 @@ class DragBox extends React.Component {
     (b1.x <= b2.x + b2.width) &&
     (b2.x + b2.width <= b1.x + b1.width) &&
     (b1.y <= b2.y) &&
-    (b2.y <= b1.y + b1.heigh) &&
+    (b2.y <= b1.y + b1.height) &&
     (b1.y <= b2.y + b2.height) &&
     (b2.y + b2.height <= b1.y + b1.height);
 
