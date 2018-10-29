3e26ddcd0d204b9f35482e21f93d7d2b7553186b
a-type <gforrest@bandwidth.com>
2018-10-29 10:59:33 -0400

    Update Callout to also use Foreground

--- stories/Callout.js
+++ stories/Callout.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Callout from 'components/Callout';
 import Icon from 'components/Icon';
+import { Grid } from 'layouts';
 
 storiesOf('Callout', module).add('types', () => (
-  <React.Fragment>
+  <Grid>
     <div>
-      <Callout content="hi there" placement="bottom-start">Hover me</Callout>
+      <Callout content="hi there" placement="bottom-start">
+        Hover me
+      </Callout>
     </div>
     <div>
       Only icon
@@ -14,5 +17,27 @@ storiesOf('Callout', module).add('types', () => (
         <Icon name="help" />
       </Callout>
     </div>
-  </React.Fragment>
+    <div>
+      Inside overflow: hidden container
+      <div style={{ overflow: 'hidden' }}>
+        <Callout content="you can still see me" placement="right">
+          <Icon name="help" />
+        </Callout>
+      </div>
+    </div>
+    <div>
+      Using this visible area as a boundary container: although the placement is
+      "top", the callout should displace to the right to avoid overlapping the
+      Storybook display area.
+      <div>
+        <Callout
+          content="doesn't overlap the left edge"
+          placement="top"
+          boundary=".sb-show-main"
+        >
+          <Icon name="help" />
+        </Callout>
+      </div>
+    </div>
+  </Grid>
 ));
