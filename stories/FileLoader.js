fe12fbdeda00c86b5a159b5b7889040a950cc52a
Zack Litzsinger <zlitzsinger@bandwidth.com>
2018-11-28 11:40:12 -0500

    added file loader to storybook

---
+++ stories/FileLoader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import FileLoader from 'components/FileLoader';
+
+storiesOf('FileLoader', module).add('types', () => (
+  <FileLoader onChange={action('fileUploaded')} />
+));
