bf0eea09974a0a3018df22aba0508a6cf313bc1f
a-type <gforrest@bandwidth.com>
2019-02-06 11:42:42 -0500

    add disabled state for file loader

--- src/components/FileLoader/FileLoader.js
+++ src/components/FileLoader/FileLoader.js
@@ -53,21 +53,23 @@ export default class FileLoader extends React.Component {
   };
 
   renderFiles = () => {
-    const { value, DropArea, Preview } = this.props;
+    const { value, DropArea, Preview, disabled } = this.props;
     if (value && value[0]) {
       return (
-        <DropArea>
+        <DropArea disabled={disabled}>
           <Icon name="fileFilled" />
           <Preview active={true}>{value[0].name}</Preview>
         </DropArea>
       );
     } else {
       return (
-        <DropArea>
+        <DropArea disabled={disabled}>
           <Icon name="file" />
           <Preview>
             DROP A FILE HERE, OR&nbsp;
-            <Link onClick={this.preventLinkClick}>CLICK TO BROWSE</Link>
+            <Link disabled={disabled} onClick={this.preventLinkClick}>
+              CLICK TO BROWSE
+            </Link>
           </Preview>
         </DropArea>
       );
