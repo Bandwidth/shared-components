07cf031ad4bb67341c7335c5fa8547060312159e
a-type <gforrest@bandwidth.com>
2019-02-08 11:59:56 -0500

    fix Link disabled state

--- src/components/Link/styles/TextLink.js
+++ src/components/Link/styles/TextLink.js
@@ -86,6 +86,12 @@ const TextLink = styled(Link)`
 
     ${({ appearFocused, icon }) =>
       appearFocused ? focusAfterStyles : icon ? 'opacity: 0;' : ''};
+
+    ${({ disabled }) =>
+      disabled &&
+      css`
+        background: ${get('colors.text.disabled')};
+      `}
   }
 
   &:hover::after,
