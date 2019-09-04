ccdf2cd8e9c974d3f1b802e2117eab5762ba32d1
Zack Litzsinger <zelitzsi@ncsu.edu>
2019-09-04 14:23:50 -0400

    esm support (#307)

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,18 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const outputFile =
   NODE_ENV === 'production' ? './dist/prod.js' : './dist/dev.js';
+const outputModule =
+  NODE_ENV === 'production' ? './dist/prod.esm.js' : './dist/dev.esm.js';
 
 module.exports = {
   input: './src/index.js',
-  output: {
+  output: [{
     file: outputFile,
     format: 'cjs',
-  },
+  }, {
+    file: outputModule,
+    format: 'esm',
+  }],
   external: id => /^react|react-dom|styled-components/.test(id),
   plugins: [
     replace({
